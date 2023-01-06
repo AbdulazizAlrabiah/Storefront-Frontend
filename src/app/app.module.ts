aebf87e8620725062f89868540a880c3e8e521e2
Abdulaziz m1 <aziz4000@gmail.com>
2023-01-06 19:09:26 +0300

    Add styling dependencies (+configure)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +18,11 @@ import { HeaderComponent } from './components/header/header.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+    NgbModule,
+    FlexLayoutModule,
+    FormsModule, 
+    ReactiveFormsModule, 
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
