8cb008dd56927b535bcda5c4a53179ac727a54f6
Abdulaziz m1 <aziz4000@gmail.com>
2023-01-06 19:10:49 +0300

    Add router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './screens/productlist/productlist.component';
+import { ProductDetailComponent as ProductDetailComponent } from './screens/productdetail/productdetail.component';
+import { CartComponent } from './screens/cart/cart.component';
+import { OrderCompletionComponent } from './screens/ordercompletion/ordercompletion.component';
+import { CreateUserComponent } from './screens/createuser/createuser.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CreateUserComponent,
+  },
+  {
+    path: 'product',
+    component: ProductListComponent,
+  },
+  {
+    path: 'product_detail/:id',
+    component: ProductDetailComponent,
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
+  },
+  {
+    path: 'cart/order_completion',
+    component: OrderCompletionComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+}
