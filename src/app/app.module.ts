a5e1bf623bc1cf2e94510c92028398af56dc6aed
Abdulaziz m1 <aziz4000@gmail.com>
2023-01-14 20:04:22 +0300

    Fix and modify app module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './screens/productlist/productlist.component';
@@ -9,10 +8,11 @@ import { CartComponent } from './screens/cart/cart.component';
 import { OrderCompletionComponent } from './screens/ordercompletion/ordercompletion.component';
 import { ProductComponent } from './components/product/product.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CreateUserComponent } from './screens/createuser/createuser.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { ProductInCartComponent } from './components/productincart/productincart.component';
 import { PaymentFormComponent } from './components/paymentform/paymentform.component';
