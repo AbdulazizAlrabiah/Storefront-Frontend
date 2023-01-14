11b1c64fefe1c02ac2a1a813fcf446ea59ab1c76
Abdulaziz m1 <aziz4000@gmail.com>
2023-01-14 19:58:00 +0300

    Implement token management

--- src/app/services/user-management.service.ts
+++ src/app/services/user-management.service.ts
@@ -7,16 +7,24 @@ import { Observable, of } from 'rxjs';
   providedIn: 'root'
 })
 export class UserManagementService {
-  // TODO: Save locally
-  static token: String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxNywidXNlcm5hbWUiOiJ3ZWZ3ZWYgd2Vmd2VmIn19.Ip7p1PyMSxuelQS_4db1ZFR-1eAGc4mM_40a5ndmt9Q";
 
   constructor(private httpClient: HttpClient) {}
 
-  static getAuthHeader(): HttpHeaders {
-    return new HttpHeaders().set('authorization', UserManagementService.token as string)
+  static getTolken(): string {
+    return localStorage.getItem('UserToken') as string;
   }
 
-  register(firstName: String, lastName: String, password: String): Observable<String> {
-    return this.httpClient.post<String>(NetworkingConfig.registerAPI, { 'firstName': firstName, 'lastName': lastName, 'password': password });
- }  
+  static getAuthHeader(): HttpHeaders {
+    return new HttpHeaders().set('authorization', UserManagementService.getTolken())
+  }
+
+  async register(firstName: String, lastName: String, password: String): Promise<void> {
+    return new Promise((resolve) => {
+      this.httpClient.post<String>(NetworkingConfig.registerAPI, { 'firstName': firstName, 'lastName': lastName, 'password': password }).subscribe( (res) => {
+        localStorage.setItem('UserToken', res as string);
+        console.log(res);
+        resolve();
+      });
+    })
+  }
 }
